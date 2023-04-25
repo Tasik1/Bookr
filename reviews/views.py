53cd4f9ad80b1e68a809ebf653d810550cd42bdc
Tasik1 <sbidaishiev@gmail.com>
2023-04-25 15:45:32 +0600

    Activity 1 of chapter 8: adding cover and sample images (error: cannot clear sample image before cover)

--- reviews/views.py
+++ reviews/views.py
@@ -4,7 +4,11 @@ from django.utils import timezone
 
 from .models import Book, Contributor, Publisher, Review
 from .utils import average_rating
-from .forms import SearchForm, PublisherForm, ReviewForm
+from .forms import SearchForm, PublisherForm, ReviewForm, BookMediaForm
+
+from io import BytesIO
+from PIL import Image
+from django.core.files.images import ImageFile
 
 
 def index(request):
@@ -131,3 +135,33 @@ def review_edit(request, book_pk, review_pk=None):
                    "related_instance": book,
                    "related_model_type": "Book"
                    })
+
+
+def book_media(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == "POST":
+        form = BookMediaForm(request.POST, request.FILES, instance=book)
+
+        if form.is_valid():
+            book = form.save(False)
+            cover = form.cleaned_data.get("cover")
+
+            if cover:
+                image = Image.open(cover)
+                image.thumbnail((300, 300))
+                image_data = BytesIO()
+                image.save(fp=image_data, format=cover.image.format)
+                image_file = ImageFile(image_data)
+                book.cover.save(cover.name, image_file)
+
+            book.save()
+            messages.success(request, "Book \"{}\" was successfully updated.".format(book))
+            return redirect("book_detail", book.pk)
+    else:
+        form = BookMediaForm(instance=book)
+
+    return render(request, "reviews/instance-form.html", {"instance": book,
+                                                          "form": form,
+                                                          "model_type": "Book",
+                                                          "is_file_upload": True})
